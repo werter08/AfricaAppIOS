e360c59a8b6babf7bcd0ced1486f96518da2a285
werter <sgot7324@gmail.com>
2025-04-20 12:25:50 +0500

    Start of developing Afrcia app

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Begench on 20.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
