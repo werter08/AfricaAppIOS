28768545c004759f8e99446888315378f0403f8e
werter <sgot7324@gmail.com>
2025-04-21 15:49:53 +0500

    Gallery view was finished

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,11 +8,59 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+
+    
+    let animals: [AnimalModel] = Bundle.main.decode("animals.json")
+    let haptic = UIImpactFeedbackGenerator(style: .medium )
+    
+    @State private var selectedAnimal: String = "lion"
+    @State var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State var gridColumn: Double = 3.0
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            
+            VStack(alignment: .center, spacing: 30) {
+                
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(.circle)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding(.vertical)
+                    .onChange(of: gridColumn) { _ in
+                        gridSwitch()
+                        
+                    }
+                
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(.circle)
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                haptic.impactOccurred()
+                            }
+                    }
+                }
+                .animation(.easeIn)
+                .onAppear {
+                    gridSwitch()
+                }
+            }
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         }
-        .frame(width: .infinity, height: .infinity)
         .background(MotionAnimationView())
         .navigationTitle("Gallery")
         
